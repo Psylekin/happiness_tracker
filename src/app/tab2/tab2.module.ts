34f34b02fde307b60e0f0bcf3074392ac38e90cb
Psylekin <bernardon@hotmail.de>
2019-04-19 17:57:04 +0200

    Implemented ngFor on Formgroup

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -2,13 +2,15 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {formatDate} from '@angular/common';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+export class Tab2Page {
+  private lifesatisfaction : FormGroup;
+  public now : Date = new Date();
+  public date : string; 
+  public timeOfDay : string;
+
+  constructor( private formBuilder: FormBuilder ) {
+    this.date = formatDate(new Date(), 'dd.MM.yyyy', 'en');
+    this.timeOfDay = formatDate(new Date(), 'HH:mm', 'en');
+    this.lifesatisfaction = this.formBuilder.group({
+      lifesatisfationValue: ['Not filled out', Validators.required],
+      date: [this.date],
+      timeOfDay: [this.timeOfDay]
+    });
+  }
+  logForm(){
+    console.log(this.lifesatisfaction.value)
+  }
+
+  
+
+}
+
