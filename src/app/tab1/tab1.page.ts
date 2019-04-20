5b8d0b1030366165724e8ab28beb7c4f6c2a89f9
Psylekin <bernardon@hotmail.de>
2019-04-20 11:50:04 +0200

    Added logService

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import {formatDate} from '@angular/common';
+import { LogService } from '../services/log.service';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {  private lifesatisfaction : FormGroup;
+export class Tab1Page {  
+  private lifesatisfaction : FormGroup;
   public now : Date = new Date();
   public date : string; 
   public timeOfDay : string;
 
-  constructor( private formBuilder: FormBuilder ) {
+  constructor( private formBuilder: FormBuilder, private logService : LogService ) {
     this.date = formatDate(new Date(), 'dd.MM.yyyy', 'en');
     this.timeOfDay = formatDate(new Date(), 'HH:mm', 'en');
     
@@ -22,9 +25,10 @@ export class Tab1Page {  private lifesatisfaction : FormGroup;
       timeOfDay: [this.timeOfDay]
     });
   }
+
   logForm(){
-    console.log(this.lifesatisfaction.value)
+    this.logService.log(this.lifesatisfaction.value)
   }
 }
 
-//TODO: Integrate LogService, Develop to http service
+//TODO: Develop to local saving service, Develop to Http Service
